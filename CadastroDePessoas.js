a9937548214a63cf19ece386fabe9568072911e6
LCBarradas <100002313+LCBarradas@users.noreply.github.com>
2022-04-14 21:53:23 -0300

    Add files via upload

---
+++ CadastroDePessoas.js
@@ -0,0 +1,29 @@
+
+let AnoEvento = 2022
+var AnoNascimento = 2010
+var Cadastro = 50
+let Limite = 100
+
+
+//verificar idade 
+if (AnoEvento - AnoNascimento <=17){
+        console.log ("Não é permitido a entrada de pessoas menores de 18 anos")
+   }else{
+    (AnoEvento - AnoNascimento >18)
+        console.log ("Digite seu nome e CPF:") 
+        
+   
+        
+}        
+//verificar quantidade de participantes
+if (Cadastro <= Limite){
+        console.log ("Cadatro realizado com sucesso!")
+    }else{
+        console.log ("Seu cadastro não pode ser realizado quantidade de participantes já foi atingida")
+
+    }
+
+
+
+
+
